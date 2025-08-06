117265e4afa2c2f0c75ed9a9206f153e0ec58644
Jaqueline Costa <scallopc@gmail.com>
2025-08-06 18:14:27 -0300

    feat: add product listing page with category filtering and product item component

--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -3,14 +3,16 @@ import Link from "next/link";
 
 import { productTable, productVariantTable } from "@/db/schema";
 import { formatCentsToBRL } from "@/helpers/money";
+import { cn } from "@/lib/utils";
 
 interface ProductItemProps {
     product: typeof productTable.$inferSelect & {
         variants: (typeof productVariantTable.$inferSelect)[];
     };
+    textContainerClassName?: string;
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = ({ product, textContainerClassName }: ProductItemProps) => {
     const firstVariant = product.variants[0];
     return (
         <Link href="/" className="flex flex-col gap-4">
@@ -21,7 +23,7 @@ const ProductItem = ({ product }: ProductItemProps) => {
                 height={200}
                 className="rounded-3xl"
             />
-            <div className="flex max-w-[200px] flex-col gap-1">
+            <div className={cn("flex max-w-[200px] flex-col gap-1", textContainerClassName)}>
                 <p className="truncate text-sm font-medium">{product.name}</p>
                 <p className="text-muted-foreground truncate text-xs font-medium">
                     {product.description}
