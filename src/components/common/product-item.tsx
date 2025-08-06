28c104a1e2ed48350a6c3948a5d53d68369adb18
Jaqueline Costa <scallopc@gmail.com>
2025-08-06 18:58:59 -0300

    feat: implement product variant page with selector, actions and item components

--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -15,15 +15,24 @@ interface ProductItemProps {
 const ProductItem = ({ product, textContainerClassName }: ProductItemProps) => {
     const firstVariant = product.variants[0];
     return (
-        <Link href="/" className="flex flex-col gap-4">
+        <Link
+            href={`/product-variant/${firstVariant.slug}`}
+            className="flex flex-col gap-4"
+        >
             <Image
                 src={firstVariant.imageUrl}
                 alt={firstVariant.name}
-                width={200}
-                height={200}
-                className="rounded-3xl"
+                sizes="100vw"
+                height={0}
+                width={0}
+                className="h-auto w-full rounded-3xl"
             />
-            <div className={cn("flex max-w-[200px] flex-col gap-1", textContainerClassName)}>
+            <div
+                className={cn(
+                    "flex max-w-[200px] flex-col gap-1",
+                    textContainerClassName,
+                )}
+            >
                 <p className="truncate text-sm font-medium">{product.name}</p>
                 <p className="text-muted-foreground truncate text-xs font-medium">
                     {product.description}
